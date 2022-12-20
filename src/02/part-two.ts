a003e8e14c70daaf123f6482706d0e1bb2b02bff
Grégory Houllier <gregory.houllier@gmail.com>
2022-12-20 21:55:17 +0000

    feat: add 02 solution

---
+++ src/02/part-two.ts
@@ -0,0 +1,58 @@
+import { EOL } from "node:os";
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+
+const POINTS_PER_MOVE = {
+  // Rock
+  A: 1,
+  X: 1,
+  // Paper
+  B: 2,
+  Y: 2,
+  // Scissors
+  C: 3,
+  Z: 3,
+} as const;
+
+const LOOSE = 0;
+const DRAW = 3;
+const WIN = 6;
+
+const POINTS_PER_FIGHT = {
+  "A X": [POINTS_PER_MOVE["A"] + WIN, POINTS_PER_MOVE["C"] + LOOSE],
+  "A Y": [POINTS_PER_MOVE["A"] + DRAW, POINTS_PER_MOVE["A"] + DRAW],
+  "A Z": [POINTS_PER_MOVE["A"] + LOOSE, POINTS_PER_MOVE["B"] + WIN],
+
+  "B X": [POINTS_PER_MOVE["B"] + WIN, POINTS_PER_MOVE["A"] + LOOSE],
+  "B Y": [POINTS_PER_MOVE["B"] + DRAW, POINTS_PER_MOVE["B"] + DRAW],
+  "B Z": [POINTS_PER_MOVE["B"] + LOOSE, POINTS_PER_MOVE["C"] + WIN],
+
+  "C X": [POINTS_PER_MOVE["C"] + WIN, POINTS_PER_MOVE["B"] + LOOSE],
+  "C Y": [POINTS_PER_MOVE["C"] + DRAW, POINTS_PER_MOVE["C"] + DRAW],
+  "C Z": [POINTS_PER_MOVE["C"] + LOOSE, POINTS_PER_MOVE["A"] + WIN],
+} as const;
+
+type Fight = keyof typeof POINTS_PER_FIGHT;
+
+export async function main(): Promise<number> {
+  const input = await readFile(resolve(__dirname, "input.txt"), {
+    encoding: "utf-8",
+  });
+  const games = input
+    .split(EOL)
+    .filter((game): game is Fight => game in POINTS_PER_FIGHT);
+
+  const scores = games.map((game) => POINTS_PER_FIGHT[game]);
+
+  const total = scores.reduce(
+    ([totalAdverser, totalMe], [scoreAdverser, scoreMe]) => [
+      totalAdverser + scoreAdverser,
+      totalMe + scoreMe,
+    ],
+    [0, 0]
+  );
+
+  return total[1];
+}
+
+main().then(console.log);
